fd797eb0988e044dac90785b79107603896cb2a3
matthew.mitchell <matt.mitchell@flatironschool.com>
2019-03-20 10:58:58 -0400

    substantial rewrite

--- school.py
+++ school.py
@@ -1,26 +1,19 @@
-import copy
-
 class School:
-
     def __init__(self, name):
         self.name = name
-        self._roster = {}
-
-    def roster(self):
-        return self._roster
-
+        self.roster = {}
     def add_student(self, name, grade):
-        if grade in self._roster:
-            self._roster[grade].append(name)
+        if grade in self.roster:
+            self.roster[grade].append(name)
         else:
-            self._roster[grade] = [name]
-        return self._roster
+            self.roster[grade] = [name]
 
+        #Alternatively, this could be written as:
+        #self.roster[grade] = self.roster.get(grade, []).append(name)
     def grade(self, grade):
-        return self._roster[grade]
-
+           return self.roster[grade]
     def sort_roster(self):
-        new_dict = copy.deepcopy(self._roster)
-        for key in new_dict:
-            new_dict[key].sort()
-        return new_dict
+            new_dict = self.roster
+            for key in new_dict:
+                new_dict[key].sort()
+            return new_dict
